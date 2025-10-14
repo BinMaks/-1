5a2af7a63e0878a1ec1467e88b1a2fbe5470986a
BinMaks <maksimmama09@mail.ru>
2025-10-14 20:43:28 +0300

    Add files via upload

---
+++ 2.12.py
@@ -0,0 +1,4 @@
+population=float(input("Введите численность населения (чел):"))
+area=float(input("Введите площадь территории (км2):"))
+density=population/area
+print(f"Плотность населения:{density:.2f} чел/км2")
