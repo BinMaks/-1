29d8b1ac12649aa4029c3d7cbc3ff6df0de67ced
BinMaks <maksimmama09@mail.ru>
2025-11-23 16:08:23 +0300

    Add files via upload

---
+++ 2.38.py
@@ -0,0 +1,12 @@
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+summa = a + b
+raznost = a - b
+proizvedenie = a * b
+chastnoe = a/b
+print(f"\n ---- Результаты ----")
+print(f"{a} + {b} = {summa}")
+print(f"{a} - {b} = {raznost}")
+print(f"{a} * {b} = {proizvedenie}")
+print(f"{a} / {b} = {chastnoe: .2f}")
+
