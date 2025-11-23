02a7807792d63a313336af53686feeaec95062e4
BinMaks <maksimmama09@mail.ru>
2025-11-23 16:02:55 +0300

    Add files via upload

---
+++ 2.37.py
@@ -0,0 +1,3 @@
+fahrenheit = float(input("Введите температуру по шкале Фаренгейта: "))
+celsius = (5/9)*(fahrenheit-32)
+print(f"{fahrenheit: .2f} градусов по Фаренгейту = {celsius: .2f} градусов по Цельсию")
