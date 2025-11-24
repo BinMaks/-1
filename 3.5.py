0c70dbcc747b3bed74cf72b5fdb1e1f54fb185bd
BinMaks <maksimmama09@mail.ru>
2025-11-24 20:33:47 +0300

    Add files via upload

---
+++ 3.5.py
@@ -0,0 +1,7 @@
+lenght = 543
+width = 130
+square_side = 130
+count_along_lenght = lenght // square_side
+count_along_width = width // square_side
+total_squares = count_along_lenght * count_along_width
+print("Количество квадратов 130*130 мм, которые можно орезать:", total_squares)
