01e6982d84f030d2cf3bee351e1bd95857dd2384
BinMaks <maksimmama09@mail.ru>
2025-12-08 20:33:47 +0300

    Add files via upload

---
+++ 4.47.py
@@ -0,0 +1,12 @@
+a = float(input("Введите a: "))
+b = float(input("Введите b: "))
+c = float(input("Введите c: "))
+if a < b < c:
+    print("Неравенство a < b < c выполняется")
+else:
+    print("Неравенство a < b < c не выполняются")
+
+if b > a > c:
+    print("Неравенство b > a > c выполняется")
+else:
+    print("Неравенство b > a > c не выполняется ")
