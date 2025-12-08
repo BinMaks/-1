852c16f11d8ef013c0be1ca26058317819a80a2c
BinMaks <maksimmama09@mail.ru>
2025-12-08 20:43:43 +0300

    Add files via upload

---
+++ 4.50.py
@@ -0,0 +1,10 @@
+num = int(input("Введите двузначное число: "))
+if '4' in str(num) or '7' in str(num):
+    print("В числе есть 4 или 7")
+else:
+    print("В числе нет 4 и 7")
+
+if '3' in str(num) or '6' in str(num) or '9' in str(num):
+    print("В числе есть 3, 6 или 9")
+else:
+    print("В чичле нет 3, 6 и 9")
