d8c76fbc1bda16b7b62e8fbcd5d893b9192b04a2
BinMaks <maksimmama09@mail.ru>
2025-12-08 21:10:40 +0300

    Add files via upload

---
+++ 4.58.py
@@ -0,0 +1,7 @@
+a = int(input("Введите a: "))
+b = int(input("Введите b: "))
+c = int(input("Введите c: "))
+if a == b or a == c or b == c:
+    print("Есть хотя бы одна пара равных чисел")
+else:
+    print("Нет пар равных чисел")
