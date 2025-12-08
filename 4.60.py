58e93793e8b0b724ed18367f9e852930f1353a72
BinMaks <maksimmama09@mail.ru>
2025-12-08 21:16:23 +0300

    Add files via upload

---
+++ 4.60.py
@@ -0,0 +1,7 @@
+growth1 = float(input("введите рост первого человека: "))
+growth2 = float(input("введите рост второго человека: "))
+growth3 = float(input("введите рост третьего человека: "))
+if growth1 == growth2 == growth3:
+    print("Рост всех трех человек одинаков")
+else:
+    print("Рост не одинаков")
