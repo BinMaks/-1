fc84ba30f47b4002bf6193ae4b7714d3049fbda0
BinMaks <maksimmama09@mail.ru>
2025-12-15 20:33:13 +0300

    Add files via upload

---
+++ 4.124.py
@@ -0,0 +1,8 @@
+mitя_age = int(input("Введите возраст Мити: "))
+vasя_age = int(input("Введите возраст Васи: "))
+if mitя_age > vasя_age:
+    print("Митя старше Васи")
+elif mitя_age < vasя_age:
+    print("Вася старше Мити")
+else:
+    print("Митя и Вася одного возраста")
