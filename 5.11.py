16ac62d098eee4554f421af761f8689d19be35a8
BinMaks <maksimmama09@mail.ru>
2025-12-16 20:32:49 +0300

    Add files via upload

---
+++ 5.11.py
@@ -0,0 +1,8 @@
+import math
+p0 = 1.29
+z = 1.25e-4
+print("Высота (м) | Плотность (кг/м³)")
+print("-! * 30")
+for h in range(0, 1001, 100):
+    p = p0 * math.exp(-z * h)
+    print(f"{h:10} | {p:15.4f}")
