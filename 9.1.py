2f37306c9950b220f5a5b656f7a35fa52bfab176
BinMaks <maksimmama09@mail.ru>
2026-01-11 11:14:48 +0300

    Add files via upload

---
+++ 9.1.py
@@ -0,0 +1,36 @@
+for i in range(5):
+    for j in range(5):
+        print("8", end=" ")
+    print()
+
+
+rows = [[1, 1, 1, 1, 1], [2, 2, 2, 2, 2], [7, 7, 7, 7, 7]]
+for row in rows:
+    for num in row:
+        print(num, end=" ")
+    print()
+
+
+for i in [10, 20, 80]:
+    for j in range(5):
+        print(i, end=" ")
+    print()
+
+
+for i in [12, 22, 82]:
+    for j in range(5):
+        print(i, end=" ")
+    print()
+
+
+row = [2, 3, 20]
+for i in range(3):
+    for num in row:
+        print(num, end=" ")
+    print()
+
+rows = [[15, 14, 3], [15, 14, 3], [15, 14, 3]]
+for row in rows:
+    for num in row:
+        print(num, end=" ")
+    print()
